7999bfa36b0db73e777363665d79cbd20d9dcbf2
Emma Repp <emmarepp.draws@gmail.com>
2022-01-01 19:33:53 -1000

    commit tree 77bae5c11ed5c895a5b85a570d0a0c6a3ba76d64

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase())!== -1
+    })
+}
+
+
+function fuzzyMatch(array, string){
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+function matchName(object,string) {
+    return object.filter(driver => driver.name === string);
+}
